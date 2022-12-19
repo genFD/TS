436949e2a34c1f67b6f8bca288ace36ded8ad44b
hkan <hermann.kan.dev@gmail.com>
2022-12-19 10:29:00 -0500

    tsv9

--- exo/first/src/Map.ts
+++ exo/first/src/Map.ts
@@ -1,3 +1,35 @@
-export class Map {}
-
-// const map =
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+export class Map {
+  // create a property and add a modifier if necessary
+  // anotate the property
+  // initialize the property inside of constructor or on the same line
+  // ----------------------------------------------------------------
+  //type of property googleMap is going to be an instance of class Map
+  // a class can be used to create an instance of this class
+  // or to be refer to as a type
+  private googleMap: google.maps.Map;
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+  // if you want to be an argument to addMarker you have to be of type Mappable
+  addMarker(item: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: item.location.lat,
+        lng: item.location.lng,
+      },
+    });
+  }
+}
--- exo/first/src/index.ts
+++ exo/first/src/index.ts
@@ -3,13 +3,4 @@ import { User } from './User';
 import { Map } from './Map';
 const user = new User();
 const company = new Company();
-console.log(user);
-console.log(company);
-
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const map = new Map('map');
